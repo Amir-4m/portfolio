44e51c868941940ec6d1183f2b680aaa93511e2c
Amir <amiri.amir30@yahoo.com>
2020-03-30 17:54:00 +0430

    project section added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
+import random
+import os
+
 from django.db import models
 
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    new_filename = random.randint(1, 3910209312)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return "products/{new_filename}/{final_filename}".format(
+        new_filename=new_filename,
+        final_filename=final_filename
+    )
+
+
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=120, blank=False, null=False)
+    description = models.TextField(null=True, default='description')
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    in_progress = models.BooleanField(default=False)
+    project_url = models.URLField()
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
--- main/views.py
+++ main/views.py
@@ -1,10 +1,8 @@
-from django.urls import reverse
 from django.utils.decorators import method_decorator
-from datetime import datetime
 from django.views.decorators.cache import cache_control, cache_page
 from django.views.generic import TemplateView
-from django.conf import settings
-from django.utils.translation import gettext_lazy as _
+
+from .models import Project
 
 
 @method_decorator(cache_control(max_age=1 * 24 * 60 * 60), name='get')  # 1 day
@@ -12,3 +10,10 @@ from django.utils.translation import gettext_lazy as _
 class HomePage(TemplateView):
     http_method_names = ['get']
     template_name = "main/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePage, self).get_context_data(**kwargs)
+        context.update({
+            'projects': Project.objects.all()
+        })
+        return context
